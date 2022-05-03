46f8773506347ca9916ec1bac619c380d748531c
Zoey <jsisavath@gmail.com>
2022-05-03 08:30:16 -0500

    edited ID functions

--- server.js
+++ server.js
@@ -58,7 +58,10 @@ function filterByQuery(query, animalsArray) {
 
 
 
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 
 
@@ -70,6 +73,14 @@ app.get('/api/animals', (req, res) => {
       results = filterByQuery(req.query, results);
     }
     res.json(results);
+  });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
   app.listen(PORT, () => {
